da699e8b58eeac2cfd36f9bedfdabed8241b0ba5
El4035 <elenats2006@gmail.com>
2025-07-15 17:06:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ TOKEN = "8111573872:AAE_LGmsgtGmKmOxx2v03Tsd5bL28z9bL3Y"
 CHAT_ID = 944484522
 bot = Bot(token=TOKEN)
 
-print("🚀 Старт: бот запустился")
-
 try:
-    bot.send_message(chat_id=CHAT_ID, text="✅ Бот запущен и работает!")
-    print("✅ Сообщение отправлено успешно")
+    bot.send_message(chat_id=CHAT_ID, text="Проверка")
+    print("✅ Сообщение отправлено")
 except Exception as e:
     print(f"❌ Ошибка Telegram: {e}")
